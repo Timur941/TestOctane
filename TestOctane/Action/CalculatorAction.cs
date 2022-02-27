ba63fd672b38c7afa213cc8d6c46270577c881fa
qwbruh <mkvantio@gmail.com>
2022-02-27 23:30:45 +0300

    initial

---
+++ TestOctane/Action/CalculatorAction.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechTalk.SpecFlow;
+
+namespace TestOctane.Action
+{
+    [Binding]
+    class CalculatorAction
+    {
+        [StepDefinition(@"успешный тест")]
+        public void SuccessfullTest()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
---
+++ TestOctane/Feature/CalculatorFeature.feature
@@ -0,0 +1,5 @@
+﻿Feature: CalculatorFeature
+	Simple calculator for adding two numbers
+
+Scenario: Add two numbers
+	Given успешный тест
